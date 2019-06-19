bb0ec5082b309cdba40acf118a11a3051097523b
Andre Luis Florencio da Silva <andre.luis.florencio@gmail.com>
2019-06-18 21:35:11 -0300

    Validaçõe para alerts e confirm

--- Readme.md
+++ Readme.md
@@ -1,5 +1,3 @@
 # Curso JavaScritp Moderno
 
 Curso da Caelum de JavaScript moderno para front e back end
-
-
--- scripts/pedePaginaIncial.js
+++ scripts/pedePaginaIncial.js
@@ -1,18 +1,22 @@
-let paginaInicial=localStorage.getItem('paginaInicial');
+const aceitouSalvar = JSON.parse(aceitouAnteriormente);
 
-if(!paginaInicial){
-    paginaInicial = prompt("Escolha a página inicial");  
-} 
+if(aceitouSalvar){
+    let paginaInicial=localStorage.getItem('paginaInicial');
 
-if(paginaInicial){
-    if (
-        paginaInicial.substring(0,7) !== 'http://' && 
-        paginaInicial.substring(0,8) !== 'https://'
-    ) {
-        paginaInicial = `http://${paginaInicial}`;
+    if(!paginaInicial){
+        paginaInicial = prompt("Escolha a página inicial");  
+    } 
+
+    if(paginaInicial){
+        if (
+            paginaInicial.substring(0,7) !== 'http://' && 
+            paginaInicial.substring(0,8) !== 'https://'
+        ) {
+            paginaInicial = `http://${paginaInicial}`;
+        }
+
+        $janelaPrincipal.src = paginaInicial;
+        $inputEndereco.value = paginaInicial;
+        localStorage.setItem('paginaInicial', paginaInicial);
     }
-
-    $janelaPrincipal.src = paginaInicial;
-    $inputEndereco.value = paginaInicial;
-    localStorage.setItem('paginaInicial', paginaInicial);
 }
