c84a57b2366445c9d2f67299f63b46d8ed97e16a
Andre Luis Florencio da Silva <andre.luis.florencio@gmail.com>
2019-06-19 22:49:40 -0300

    Corrigindo bug de atribuir valor para module

--- scripts/pedeInfoIniciais/pedeAceitouSalvar.js
+++ scripts/pedeInfoIniciais/pedeAceitouSalvar.js
@@ -1,7 +1,7 @@
 import aceitouSalvar from '/scripts/storage/aceitouSalvar.js'
 
 if(aceitouSalvar === null){
-     aceitouSalvar = confirm('Você aceita que a gente salve suas informações?');
+    const aceitouSalvar = confirm('Você aceita que a gente salve suas informações?');
     
     if (!aceitouSalvar) {
         alert('Você pode mudar isso na página de configurações');
@@ -10,5 +10,3 @@ if(aceitouSalvar === null){
     localStorage.setItem('aceitouSalvar', aceitouSalvar);
     
 }
-
-export default aceitouSalvar
