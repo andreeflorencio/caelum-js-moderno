1953fc75cca9839995ae74e0a444823e264f0029
Andre Luis Florencio da Silva <andre.luis.florencio@gmail.com>
2019-06-21 22:21:06 -0300

    Atualizando URL resumida e completa

--- scripts/navagacao/barraEndereco.js
+++ scripts/navagacao/barraEndereco.js
@@ -1,5 +1,14 @@
-$janelaPrincipal.onload = exibeEnderecoNaBarra;
+$inputEndereco.onfocus = exibeEnderecoCompleto;
+$inputEndereco.onblur = exibeEnderecoResumido;
 
-function exibeEnderecoNaBarra(){
+$janelaPrincipal.onload = exibeEnderecoResumido;
+
+function exibeEnderecoCompleto(){
     $inputEndereco.value = $janelaPrincipal.contentWindow.location.href;
+};
+
+function exibeEnderecoResumido() {
+    const url = new URL($janelaPrincipal.contentWindow.location.href)
+    const enderecoResumido = url.hostname
+    $inputEndereco.value = enderecoResumido;
 };
