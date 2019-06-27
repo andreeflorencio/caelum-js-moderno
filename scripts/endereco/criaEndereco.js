e9d9992e79e8896b6b04585cafbb74da94dffe88
Andre Luis Florencio da Silva <andre.luis.florencio@gmail.com>
2019-06-26 21:54:15 -0300

    Criando uma função com constructor

---
+++ scripts/endereco/criaEndereco.js
@@ -0,0 +1,23 @@
+export function criaEndereco(endereco){
+    
+    const url = new URL(endereco)
+
+    let enderecoCompleto;
+    if(url.hostname === 'localhost'){
+        enderecoCompleto = url.pathname.replace("/","");
+    } else {
+        enderecoCompleto = url.href
+    };
+
+    let enderecoResumido;
+    if(url.hostname === 'localhost'){
+        enderecoResumido = url.pathname.replace("/","");
+    } else {
+        enderecoResumido = url.hostname
+    };
+
+    return {
+        urlCompleta: enderecoCompleto,
+        urlResumida: enderecoResumido
+    };
+};
